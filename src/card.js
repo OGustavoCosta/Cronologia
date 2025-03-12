002e5ab0124d5c762a048e165c709758d7f57556
OGustavoCosta <gustavosncosta@gmail.com>
2025-03-12 18:15:45 -0300

    add .bdml

---
+++ src/card.js
@@ -0,0 +1,29 @@
+export class Card{
+    constructor(img, title, date, shortDescription, description, source, difficult, verified){
+        this.img = img
+        this.title = title
+        this.date = date
+        this.shortDescription = shortDescription
+        this.description = description
+        this.source = source
+        this.difficult = difficult
+        this.verified = verified
+    }
+
+
+    list(){
+
+    }
+
+    create(){
+
+    }
+
+    update(){
+
+    }
+
+    delete(){
+
+    }
+}
