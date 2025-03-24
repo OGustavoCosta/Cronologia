637c9db46232c23414ee3c42937871eac299612d
OGustavoCosta <gustavosncosta@gmail.com>
2025-03-24 16:21:15 -0300

    add module js

--- src/http/server.mjs
+++ src/http/server.mjs
@@ -2,6 +2,10 @@ import Fastify, { fastify } from "fastify"
 
 const server = Fastify()
 
+server.get('/', (request, reply) =>{
+    reply.send( 'olá')
+})
+
 server.listen({port: 3333, host: '0.0.0.0'}).then(() =>{
     console.log('servidor inciado')
 })
